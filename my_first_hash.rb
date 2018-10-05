3153084370ad49c30d3eb2ae6d7e92d86d7def0d
Yu Li <yli0607x@gmail.com>
2018-10-05 14:39:34 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["value3"]
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -33,13 +33,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"]= 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"]= 2
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
