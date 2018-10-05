5bf107b9314e34ba859c8e0f1c4e4b62be34f36b
Yu Li <yli0607x@gmail.com>
2018-10-05 14:37:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest[value3]
+shipping_manifest["value3"]
 end
 
 def adding
